7cfe3d8e6f99be9d870f3875ddbc5544847fd6e9
Ramarko Bhattacharya <ramarkob@gmail.com>
2025-07-29 09:29:59 -0500

    Added Github Actions Deployment

--- DateTime.hs
+++ DateTime.hs
@@ -14,31 +14,39 @@ if' False _ v = v
 
 -- Struct to store Dates
 data Date = Date
-                { getMonth :: Int
-                , getDay :: Int
-                , getYear :: Int }
-                deriving (Eq)
+  { getMonth :: Int,
+    getDay :: Int,
+    getYear :: Int
+  }
+  deriving (Eq)
 
 instance Ord Date where
-    (<=) d1 d2 = if' (getYear d1 <= getYear d2)
-        True (if' (getMonth d1 <= getMonth d2)
-            True (getDay d1 <= getDay d2))
+  (<=) d1 d2 =
+    if'
+      (getYear d1 <= getYear d2)
+      True
+      ( if'
+          (getMonth d1 <= getMonth d2)
+          True
+          (getDay d1 <= getDay d2)
+      )
 
 instance Show Date where
-    show (Date m d y) = show m ++ "/" ++ show d ++ "/" ++ show y
+  show (Date m d y) = show m ++ "/" ++ show d ++ "/" ++ show y
 
 -- Struct to store Times
 data Time = Time
-                { getHour ::Int
-                , getMinute :: Int }
-                deriving (Eq, Ord)
+  { getHour :: Int,
+    getMinute :: Int
+  }
+  deriving (Eq, Ord)
 
 instance Show Time where
-    show (Time h m) = show h ++ ":" ++ show m
+  show (Time h m) = show h ++ ":" ++ show m
 
 -- Constructs Date from current time
 today :: IO Date
-today = fmap ((\(y, m, d) -> Date m d (fromIntegral y)) . (toGregorian . localDay  . zonedTimeToLocalTime)) getZonedTime
+today = fmap ((\(y, m, d) -> Date m d (fromIntegral y)) . (toGregorian . localDay . zonedTimeToLocalTime)) getZonedTime
 
 -- Constructs Time from current time
 rightNow :: IO Time
@@ -47,19 +55,19 @@ rightNow = fmap ((\(TimeOfDay hr minute _) -> Time hr minute) . localTimeOfDay .
 -- Handles overflowing dates to real dates
 toDate :: Int -> Int -> Int -> Date
 toDate m d y
-    | d > daysInMonth m y = toDate nm (d - daysInMonth m y) ny
-    | d < 1 = toDate pm (daysInMonth pm py + d) py
-    | otherwise = Date m d y
-    where
-        (nm, ny) = nextMonth (m, y)
-        (pm, py) = prevMonth (m, y)
+  | d > daysInMonth m y = toDate nm (d - daysInMonth m y) ny
+  | d < 1 = toDate pm (daysInMonth pm py + d) py
+  | otherwise = Date m d y
+  where
+    (nm, ny) = nextMonth (m, y)
+    (pm, py) = prevMonth (m, y)
 
 -- Check if a date is valid
 checkValidDate :: Int -> Int -> Int -> Maybe Date
 checkValidDate m d y
-    | m < 0 || m > 12  = Nothing
-    | d < 0 || d > daysInMonth m y = Nothing
-    | otherwise = Just $ Date m d y
+  | m < 0 || m > 12 = Nothing
+  | d < 0 || d > daysInMonth m y = Nothing
+  | otherwise = Just $ Date m d y
 
 -------------------------------------------------------------------------------
 -- Day of Week Functions
@@ -86,21 +94,21 @@ dayBump _ _ = 0
 
 -- Get Weekday of a day (0-Sunday, 1-Monday ... 6-Saturday)
 weekday :: Date -> Int
-weekday (Date m d y ) = mod (y - 1900 + dayBump m y + d + div y 4) 7
+weekday (Date m d y) = mod (y - 1900 + dayBump m y + d + div y 4) 7
 
 -------------------------------------------------------------------------------
 -- Month Functions
 
 -- Get Weekday of first day in month
 firstOfMonth :: Int -> Int -> Int
-firstOfMonth m y =  weekday $ Date m 1 y
+firstOfMonth m y = weekday $ Date m 1 y
 
 -- Get number of days in month
 daysInMonth :: Int -> Int -> Int
 daysInMonth m y
-    | m == 2 = if' (leapYear y) 29 28
-    | m `elem` [9, 4, 6, 11] = 30
-    | otherwise = 31
+  | m == 2 = if' (leapYear y) 29 28
+  | m `elem` [9, 4, 6, 11] = 30
+  | otherwise = 31
 
 -------------------------------------------------------------------------------
 -- Change Date
@@ -109,22 +117,22 @@ daysInMonth m y
 next :: Date -> Date
 next (Date 12 31 y) = Date 1 1 (y + 1)
 next (Date m d y)
-    | d == daysInMonth m y = Date (m + 1) 1 y
-    | otherwise = Date  m (d + 1)  y
+  | d == daysInMonth m y = Date (m + 1) 1 y
+  | otherwise = Date m (d + 1) y
 
 -- Get Previous Day
 prev :: Date -> Date
 prev (Date 1 1 y) = Date 12 31 (y - 1)
 prev (Date m d y)
-    | d == 1 = Date (m - 1) (daysInMonth (m - 1) y) y
-    | otherwise = Date  m (d - 1) y
+  | d == 1 = Date (m - 1) (daysInMonth (m - 1) y) y
+  | otherwise = Date m (d - 1) y
 
 -- Move Date through Time
 move :: Date -> Int -> Date
 move date i
-    | i > 0 = move (next date) (i - 1)
-    | i < 0 = move (prev date) (i + 1)
-    | otherwise = date
+  | i > 0 = move (next date) (i - 1)
+  | i < 0 = move (prev date) (i + 1)
+  | otherwise = date
 
 -- Get Next Week
 nextWeek :: Date -> Date
@@ -136,32 +144,45 @@ prevWeek date = move date (-7)
 
 -- Class Type to handle prevMonth and nextMonth for Date and (Int, Int)
 class Monthable a where
-    -- Get Previous Month
-    prevMonth :: a -> a
+  -- Get Previous Month
+  prevMonth :: a -> a
 
-    -- Get Next Month
-    nextMonth :: a -> a
+  -- Get Next Month
+  nextMonth :: a -> a
 
 instance Monthable Date where
-    prevMonth (Date 1 d y) = Date 12 (min d 31) (y - 1)
-    prevMonth (Date m d y) = Date (m - 1) (min d (daysInMonth (m - 1) y)) y
+  prevMonth (Date 1 d y) = Date 12 (min d 31) (y - 1)
+  prevMonth (Date m d y) = Date (m - 1) (min d (daysInMonth (m - 1) y)) y
 
-    nextMonth (Date 12 d y) = Date 1 (min d 31) (y + 1)
-    nextMonth (Date m d y) = Date (m + 1) (min d (daysInMonth (m + 1) y)) y
+  nextMonth (Date 12 d y) = Date 1 (min d 31) (y + 1)
+  nextMonth (Date m d y) = Date (m + 1) (min d (daysInMonth (m + 1) y)) y
 
 instance Monthable (Int, Int) where
-    prevMonth (1, y) = (12, y - 1)
-    prevMonth (m, y) = (m - 1, y)
+  prevMonth (1, y) = (12, y - 1)
+  prevMonth (m, y) = (m - 1, y)
 
-    nextMonth (12, y) = (1, y + 1)
-    nextMonth (m, y) = (m + 1, y)
+  nextMonth (12, y) = (1, y + 1)
+  nextMonth (m, y) = (m + 1, y)
 
 -------------------------------------------------------------------------------
 -- String Functions
 
 monthToStr :: Int -> String
-monthToStr n = ["January", "February", "March", "April", "May", "June",
-              "July", "August", "September", "October", "November", "December"] !! (n - 1)
+monthToStr n =
+  [ "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ]
+    !! (n - 1)
 
 weekdays :: [String]
 weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
@@ -179,6 +200,8 @@ genWeek :: Date -> [Date]
 genWeek date = [move date (i - weekday date) | i <- [0 .. 6]]
 
 genMonth :: Date -> [Date]
-genMonth (Date m _ y) = concat
+genMonth (Date m _ y) =
+  concat
     [genWeek (move first (i * 7)) | i <- [0 .. 5], 1 + (i * 7) <= weekday first + daysInMonth m y]
-    where first = Date m 1 y
+  where
+    first = Date m 1 y
--- Main.hs
+++ Main.hs
@@ -2,39 +2,31 @@
 
 module Main where
 
-import Language.Javascript.JSaddle.Warp as JSaddle
-import Miso
-
 import DateTime
+import Miso
+import Miso.String (MisoString, ms)
 import Model
-import View
 import Update
+import View
 
-runApp :: JSM () -> IO ()
-runApp =
-  JSaddle.run 8000
-  -- https://hackage.haskell.org/package/jsaddle-warp-0.9.8.2
-
---------------------------------------------------------------------------------
-
+-- For GHCJS, we don't need jsaddle-warp
 main :: IO ()
 main = do
-  css <- readFile "cal.css"
+  -- You'll need to embed the CSS or load it differently for static deployment
+  let css = "" -- You can embed your CSS content here as a string
   let env = Env css
 
   t <- today
   n <- rightNow
 
-  putStrLn ("Today is: " ++ show t)
-  putStrLn "http://localhost:8000\n"
-
-  runApp $ startApp App
-    { initialAction = OnLoad
-    , model         = initialModel t n
-    , update        = updateModel
-    , view          = viewModel env
-    , events        = defaultEvents
-    , subs          = [keyboardSub keysToAction, arrowsSub arrowsToAction, every 60000 TimeUpdate]
-    , mountPoint    = Nothing -- mount point (Nothing defaults to 'body')
-    , logLevel      = Off     -- used during prerendering to if VDOM/DOM in sync
-    }
+  startApp
+    App
+      { initialAction = OnLoad,
+        model = initialModel t n,
+        update = updateModel,
+        view = viewModel env,
+        events = defaultEvents,
+        subs = [keyboardSub keysToAction, arrowsToAction, every 60000 TimeUpdate],
+        mountPoint = Nothing,
+        logLevel = Off
+      }
